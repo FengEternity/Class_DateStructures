fb52e2e471043a74c6ffe61c7e7dbe0aa7e33cf2
Monty _L <feng_life@foxmail.com>
2022-10-31 22:21:04 +0800

    10/31 second commit

--- 3_2.c
+++ 3_2.c
@@ -5,8 +5,27 @@ typrdef enum { true = 1,
                false = 0 } bool;
 
 // 循环队列结构体定义
-typedef struct
+typedef struct queue
 {
-    /* data */
-};
+    int front;
+    int rear;
+    int maxSize;
+    ElemType *element;
+} Queue;
+
+// 创建一个能容纳 mSize 个单元的空列表
+void create(Queue *Q, int mSize)
+{
+    Q->maxSize = mSize;
+    Q->element = (ElemType *)malloc(sizeof(ElemType) * mSize);
+    Q->front = Q->rear = 0;
+}
+
+// 销毁一个已存在的队列，即释放队列占用的数组空间
+void Destroy(Queue *Q)
+{
+    Q->maxSize = 0;
+    free(Q->element);
+    Q->front = Q->rear = -1;
+}
 
