78ca8f69ffb4598eb85cabd0336548f6cc98eb62
Monty _L <feng_life@foxmail.com>
2022-11-24 21:43:49 +0800

    11/24 commit

--- test.c
+++ test.c
@@ -1,8 +1,27 @@
 #include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    printf("win\n");
-    system("pause");
+
+void days(int m, int n);
+
+int main() {
+    int m, n;
+    printf_s("Please enter the year-month in the format (2021-5):");
+    scanf_s("%d-%d", &m, &n);
+    days(m, n);
     return 0;
+}
+
+void days(int m, int n) {
+    if (n == 4 || n == 6 || n == 8 || n == 11) {
+        printf("day = 30");
+    }
+    if (n == 1 || n == 3 || n == 5 || n == 7 || n == 9 || n == 10 || n == 12) {
+        printf("day = 31");
+    }
+    if (n == 2) {
+        if ((m % 400 == 0) || (m % 4 == 0 && m % 100 != 0)) {
+            printf("day = 29");
+        } else {
+            printf("day = 28");
+        }
+    }
 }
